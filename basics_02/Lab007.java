8b4a34649eb030b7c0470c92e0b38055f421f953
unknown <prasuknarsing981@gmail.com>
2024-04-14 12:58:13 +0530

    Basics 02

---
+++ basics_02/Lab007.java
@@ -0,0 +1,32 @@
+package src.basics_02;
+
+public class Lab007 {
+	
+	 // Author - Prasuk Narsing
+	 // Program - Showcase difference between println vs print
+	
+	public static void main(String[] args) {
+		
+	
+		
+	System.out.println(" Hellow word");
+	System.out.println("prasuk");
+	
+	int age = 34;
+	
+    // %d ->  any integer - byte, short, int or long
+    // %s -> String
+    // %c -> char
+    // %f -> float
+	
+	System.out.printf(" Your Age is %d ",age);
+	
+
+	
+	
+	}
+		
+		
+	}
+
+
---
+++ basics_02/Lab015.java
@@ -0,0 +1,16 @@
+package src.basics_02;
+
+public class Lab015 {
+	public static void main(String[] args) {
+		
+		 char c = '\n'; // New line / Next Line
+	        char c1 = '\t'; // Add Tab
+	        char c2 = '\b'; // Backlash, delete 1 char
+	        char c3 = '\r'; // Backlash, delete 1 char
+	        System.out.println("Prasuk" + c3 + "Narsing");
+	        System.out.println("Prasuk\tnarsing");
+	        System.out.println("Prasu\"k");
+	        System.out.println("Prasu\'k");
+	}
+
+}
